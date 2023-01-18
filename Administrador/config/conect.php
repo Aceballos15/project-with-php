2e151ebfb9295202b64d8865dcd031f67a84e8db
Aceballos15 <ceballoscardonaalexander@gmail.com>
2023-01-18 14:14:32 -0500

    quitar if

--- Administrador/config/conect.php
+++ Administrador/config/conect.php
@@ -6,13 +6,8 @@ $db_user= $_ENV['DB_USER'];
 $db_password= $_ENV['DB_PASSWORD'];
 
 $dsn = 'mysql:dbname='.$db_name.';host='.$db_host.';'
-if($dsn){
-        $conexion = new PDO($dsn, $db_user, $db_password);
-        // echo "conexion establecida";
-  
-    }else{
-        echo "conexion fallida";
-    }
-    
+
+    $conexion = new PDO($dsn, $db_user, $db_password);
+    // echo "conexion establecida";
 ?> 
  
